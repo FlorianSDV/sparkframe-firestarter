59da7318e0424bfad1bc7a751690d38469a007b1
Florian Riecker- de Vries <florian-de-vries@hotmail.com>
2026-03-28 22:13:27 +0100

    Remove comments

--- public/index.php
+++ public/index.php
@@ -1,15 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
-use App\Controller\MainController;
-use Sparkframe\Bootstrap\Globals;
-use Sparkframe\Bootstrap\Router;
-
 require_once __DIR__ . '/../src/Bootstrap/appstart.php';
-
-// We gaan hier de bootstrapper aanzetten.
-// Die is verantwoordelijk voor alles om de app werkend te krijgen
-// Globale variabelen klaarzetten, Database connectie, router
-// Dan vertelt de router ons welke method we moeten callen
-// Die gaan we invoken.
-// We doen een echo op de response.
