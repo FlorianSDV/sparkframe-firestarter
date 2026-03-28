e3300aec45e38ffa23ae43cb41ea4c4b6f025ed0
Florian Riecker- de Vries <93703843+FlorianSDV@users.noreply.github.com>
2026-03-28 22:38:22 +0100

    Update Entities

--- src/Entity/NoteEntity.php
+++ src/Entity/NoteEntity.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use Sparkframe\Attributes\Column;
+use Sparkframe\Attributes\Primary;
 use Sparkframe\Entity\Entity;
 
 class NoteEntity extends Entity
 {
     public const string ID = 'id';
     public const string TEXT = 'text';
+
+    #[Primary]
     public int $id;
+    
+    #[Column]
     public string $text;
-    protected const array COLUMN_DESCRIPTIONS = [
-        'id' => ['int', 'primary'],
-        'text' => ['string']
-    ];
 }
